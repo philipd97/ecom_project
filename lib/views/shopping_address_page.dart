0ae0fa2621e1f7aecf6e1204e590e146ae73a2d8
philipd97 <phildkh97@gmail.com>
2023-03-09 06:03:09 +0800

    add shopping address page

---
+++ lib/views/shopping_address_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ShoppingAddressPage extends StatelessWidget {
+  static const routeName = 'shopping_address_page';
+
+  const ShoppingAddressPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
