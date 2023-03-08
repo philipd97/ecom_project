e29983a7d1f4f621b9384740344376ff32307ded
philipd97 <phildkh97@gmail.com>
2023-03-09 05:51:04 +0800

    refactor custom rounded button configuration

--- lib/widgets/custom_rounded_button.dart
+++ lib/widgets/custom_rounded_button.dart
@@ -8,6 +8,7 @@ class CustomRoundedButton extends StatelessWidget {
   final EdgeInsets? padding;
   final Color textColor;
   final Color backgroundColor;
+  final bool needWidth;
 
   const CustomRoundedButton({
     super.key,
@@ -16,30 +17,35 @@ class CustomRoundedButton extends StatelessWidget {
     this.textColor = Colors.white,
     this.backgroundColor = ColorName.blueBase,
     this.padding,
+    this.needWidth = true,
   });
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: 100.w,
-      child: TextButton(
-        onPressed: onPressed,
-        style: TextButton.styleFrom(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(10.0),
-          ),
-          padding: EdgeInsets.symmetric(vertical: 2.5.h),
-          backgroundColor: backgroundColor,
+    final textBtn = TextButton(
+      onPressed: onPressed,
+      style: TextButton.styleFrom(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0),
         ),
-        child: Text(
-          label,
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
-            fontSize: 16.0,
-            color: textColor,
-          ),
+        padding: padding ?? EdgeInsets.symmetric(vertical: 2.5.h),
+        backgroundColor: backgroundColor,
+      ),
+      child: Text(
+        label,
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 16.0,
+          color: textColor,
         ),
       ),
     );
+    if (needWidth) {
+      return SizedBox(
+        width: 100.w,
+        child: textBtn,
+      );
+    }
+    return textBtn;
   }
 }
