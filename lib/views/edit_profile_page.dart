5844cd82ab6e9c1535124d33aba2440b712b189d
philipd97 <phildkh97@gmail.com>
2023-03-09 05:56:05 +0800

    add edit_profile page

---
+++ lib/views/edit_profile_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class EditProfilePage extends StatelessWidget {
+  static const routeName = 'edit_profile';
+
+  const EditProfilePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
