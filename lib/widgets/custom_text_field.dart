ab66e9148f31b14489077611031f792334656353
philipd97 <phildkh97@gmail.com>
2023-03-15 06:27:48 +0800

    refactor to hook widget and add clear button

--- lib/widgets/custom_text_field.dart
+++ lib/widgets/custom_text_field.dart
@@ -1,9 +1,11 @@
 import 'package:ecom_project/constants/text_constant.dart';
 import 'package:ecom_project/gen/colors.gen.dart';
 import 'package:ecom_project/widgets/blue_tappable_text.dart';
+import 'package:ecom_project/widgets/buttons/clear_button.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 
-class CustomTextField extends StatefulWidget {
+class CustomTextField extends HookWidget {
   final TextEditingController controller;
   final IconData iconData;
   final String label;
@@ -21,65 +23,59 @@ class CustomTextField extends StatefulWidget {
     required this.textInputType,
     required this.textInputAction,
     this.maxLine = 1,
-    this.minLines =1,
+    this.minLines = 1,
     this.obscureText = false,
   });
 
   @override
-  State<CustomTextField> createState() => _CustomTextFieldState();
-}
-
-class _CustomTextFieldState extends State<CustomTextField> {
-  late final hideText = ValueNotifier<bool>(widget.obscureText);
-
-  @override
-  void dispose() {
-    hideText.dispose();
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return ValueListenableBuilder(
-      valueListenable: hideText,
-      builder: (_, hider, __) {
-        return TextField(
-          minLines: widget.minLines,
-          maxLines: widget.maxLine,
-          style: const TextStyle(
-            color: Colors.black,
-            fontWeight: FontWeight.w600,
-          ),
-          obscureText: hider,
-          keyboardType: widget.textInputType,
-          textInputAction: widget.textInputAction,
-          decoration: InputDecoration(
-            suffixIcon: widget.obscureText
-                ? BlueTappableText(
-                    label: hider ? TextConstants.show : TextConstants.hide,
-                    onPressed: () => hideText.value = !hider,
-                  )
+    final hideText = useState<bool>(obscureText);
+    final hasText = useState<bool>(false);
+
+    useEffect(() {
+      controller.addListener(() {
+        hasText.value = controller.text.trim().isNotEmpty;
+      });
+    }, [controller]);
+
+    return TextField(
+      controller: controller,
+      minLines: minLines,
+      maxLines: maxLine,
+      style: const TextStyle(
+        color: Colors.black,
+        fontWeight: FontWeight.w600,
+      ),
+      obscureText: hideText.value,
+      keyboardType: textInputType,
+      textInputAction: textInputAction,
+      decoration: InputDecoration(
+        suffixIcon: obscureText
+            ? BlueTappableText(
+                label: hideText.value ? TextConstants.show : TextConstants.hide,
+                onPressed: () => hideText.value = !hideText.value,
+              )
+            : hasText.value
+                ? ClearButton(onTap: controller.clear)
                 : null,
-            label: Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Icon(
-                  widget.iconData,
-                  color: ColorName.greySecondary,
-                ),
-                const SizedBox(width: 10),
-                Text(
-                  widget.label,
-                  style: const TextStyle(
-                    fontWeight: FontWeight.w600,
-                    color: ColorName.greySecondary,
-                  ),
-                ),
-              ],
+        label: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Icon(
+              iconData,
+              color: ColorName.greySecondary,
             ),
-          ),
-        );
-      },
+            const SizedBox(width: 10),
+            Text(
+              label,
+              style: const TextStyle(
+                fontWeight: FontWeight.w600,
+                color: ColorName.greySecondary,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
